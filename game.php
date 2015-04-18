10b8dc42185cc19c1e645e10dc53e47af56072d7
klhall1987 <klhall1987@gmail.com>
2015-04-18 12:34:15 -0400

    I'm getting there.

--- game.php
+++ game.php
@@ -4,37 +4,37 @@ class Game {
 
     public $name;
 
-    public $roll = [];
+    public $roll;
     
     //the first nine frames, bowler can roll up to twice on this frame. 
-    public $frame = 9;
-    
-    //tenthframe since bowler will have possiblity to roll three times on this frame. 
-    public $tenthframe;
+    public $frame; 
 
-    //bowlers name.
+    
+
+    //bowlers name constructor.
     public function __construct( $name ) {
         $this->name = $name;
     }
 
     public function bowl( array $num_pins ) {
+
         //set the number of pins knocked down into the roll array. 
         $this->roll = $num_pins;
-        var_dump($num_pins);
-    }
 
+        }
     public function score() {
 
         $score = 0;
-
-        //logic for the first nine frames bowler. 
-       for($this->frame = 1; $this->frame <= 2; $this->frame++){
-            if($this->roll == 10 && $this->frame == 1){
+        
+        if ($this->roll[0] == 10){
                 $score = $score + 10;
-            }
-        }
+        } elseif ( $this->roll[0] != 10 ) {
+            $score = $this->roll[0] + $this->roll[1];
+        }   
+
 
     // Calculate Score Here
+
     return $score;
     }
 
