51897e5412bb5733a0f1e126aeb5c77afd24604e
klhall1987 <klhall1987@gmail.com>
2015-04-21 20:53:32 -0400

    Quick up date and start of my logic. I should finish this up tomorrow.

--- game.php
+++ game.php
@@ -10,6 +10,9 @@ class Game {
     //the first nine frames, bowler can roll up to twice on this frame. 
     public $frame; 
 
+    //The last frame, bolwer can roll up to three times in this frame. 
+    public $last_frame;
+
     //bowlers name constructor.
     public function __construct( $name ) {
         $this->name = $name;
@@ -23,11 +26,20 @@ class Game {
         //splitting the roll array into chunks of two to emulate frames.  
         $this->frame = array_chunk( $this->roll , 2 );
 
+        //to use for the last frame with up to 3 values.
+        $this->last_frame = array_chunk( $this->roll , 3 );
+
         }
     public function score() {
 
         $score = 0;
-        
+
+        foreach($this->frame as $value){
+            if(in_array(10, $value)){
+                $score = $score + 10;
+            }
+        }
+
         //testing to see if this works. 
         var_dump( $this->frame );
 
